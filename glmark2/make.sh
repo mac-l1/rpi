7c42023f3c94f83c2da3097c20e42a00880631a7
mac-l1 <mac.level1@gmail.com>
2015-12-12 06:19:26 +0000

    add installation of basic build tools in make.sh script

--- glmark2/make.sh
+++ glmark2/make.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# install some basic build tools 
+sudo apt-get install -y --force-yes build-essential autoconf libtool pkg-config xutils-dev
+
+# configure and build
 ./waf configure --enable-glesv2 /home/firefly/rpi/glmark2/data --prefix=/usr/local/
 ./waf
 sudo ./waf install --destdir=/
